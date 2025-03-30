8d4174814108ebd50d97fa1953a5194c79f4332f
OrapelengN <portiaoran@gmail.com>
2025-03-30 14:48:39 -0700

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # 1-install_a_package.pp
-# This manifest installs Flask 2.1.0 and Werkzeug 2.1.x using pip3.
+# This manifest installs Flask 2.1.0 and Werkzeug 2.1.0 using pip3 and creates a script to output only the Flask version.
 
 package { 'Werkzeug==2.1.0':
   ensure   => 'installed',
@@ -10,4 +10,10 @@ package { 'Flask==2.1.0':
   ensure   => 'installed',
   provider => pip3,
   require  => Package['Werkzeug==2.1.0'],
+}
+
+file { '/root/get_flask_version.py':
+  ensure  => present,
+  content => "#!/usr/bin/python3\nimport flask\n\nprint(f'Flask {flask.__version__}')\n",
+  mode    => '0755',
 }
---
+++ 0x0A-configuration_management/get_flask_version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# get_flask_version.py
+import flask
+
+print(f"Flask {flask.__version__}")

c3c15cf810d8733b7e1532ea2844c63cec193c15
OrapelengN <portiaoran@gmail.com>
2025-03-31 06:01:41 -0700

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,20 @@
-$home = $facts['home']
+# Configure /etc/ssh/ssh_config for passwordless login.
 
 file_line { 'Declare identity file':
   ensure => present,
-  path   => "${home}/.ssh/config",
+  path   => '/etc/ssh/ssh_config',
   line   => '  IdentityFile ~/.ssh/school',
   match  => '  IdentityFile',
 }
 
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => "${home}/.ssh/config",
+  path   => '/etc/ssh/ssh_config',
   line   => '  PasswordAuthentication no',
   match  => '  PasswordAuthentication',
 }
 
-file { "${home}/.ssh/config" :
+file { '/etc/ssh/ssh_config' :
   ensure => file,
   owner => 'root',
   group => 'root',
@@ -23,11 +23,4 @@ file { "${home}/.ssh/config" :
     File_line['Declare identity file'],
     File_line['Turn off passwd auth'],
   ]
-}
-
-file { "${home}/.ssh":
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0700',
 }

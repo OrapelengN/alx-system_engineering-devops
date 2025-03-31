5ab8dae4f25c04b99e225acb6a8eb6b356d5d05b
OrapelengN <portiaoran@gmail.com>
2025-03-31 06:15:47 -0700

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,28 +1,34 @@
 # Configure /etc/ssh/ssh_config for passwordless login.
 
+exec { 'remove_duplicate_identityfile':
+  command => "sed -i '/^  IdentityFile/d' /etc/ssh/ssh_config",
+  onlyif  => "grep -c '^  IdentityFile' /etc/ssh/ssh_config | grep -q '[2-9]'",
+  before  => File_line['Declare identity file'],
+}
+
 file_line { 'Declare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '  IdentityFile ~/.ssh/school',
-  match  => '  IdentityFile',
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '  IdentityFile ~/.ssh/school',
+  match   => '^  IdentityFile',
   replace => true,
 }
 
 file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  match  => 'PasswordAuthentication no$',
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication no$',
   replace => true,
 }
 
-file { '/etc/ssh/ssh_config' :
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
+file { '/etc/ssh/ssh_config':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
   require => [
     File_line['Declare identity file'],
     File_line['Turn off passwd auth'],
-  ]
+  ],
 }

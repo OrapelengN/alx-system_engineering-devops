6358b9c9038bac9ce116d641ae7c7cb447b3cd5b
OrapelengN <portiaoran@gmail.com>
2025-04-03 08:10:08 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -36,4 +36,4 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1]))i)
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
